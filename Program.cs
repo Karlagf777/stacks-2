8bbbca32dba115bdd6f61a15185570ce73be2541
KarlaMinely7 <karla.gf.upaep@gmail.com>
2026-05-21 14:18:07 +0000

    descripcion de cambios

--- Program.cs
+++ Program.cs
@@ -6,12 +6,18 @@ class Program
     static void Main()
     {
         Stack<string> pedidos= new Stack<string>();
-
-        //push agregar pedidos a la pila
-        pedidos.Push("Tacos");
-        pedidos.Push("Tortas");
+        pedidos.Push("Tacos"); 
+        pedidos.Push("Torta");
         pedidos.Push("Agua");
+        //Peek = VER el tope sin quitarlo
+        string actual = pedidos.Peek();
+        Console.WriteLine($"Tope actual: {actual}"); //Agua
+        Console.WriteLine(pedidos.Count); // 3 (no cambió)
+        // Pop = QUITAR y devolver el tope 
+        string retirado = pedidos.Pop();
+        Console.WriteLine($"Se retiró: {retirado}"); //Agua
+        Console.WriteLine(pedidos.Count); // 2 (si cambió)
 
-        Console.WriteLine(pedidos.Count);
+        Console.WriteLine(pedidos.Peek()); //Torta
     }
 }
